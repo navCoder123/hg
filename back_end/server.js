215ddae18f0936bcb66c4fe1693f7cbbf650b22f
navCoder123 <88864489+navCoder123@users.noreply.github.com>
2025-11-29 14:44:31 +0530

    update
    new update

--- back_end/server.js
+++ back_end/server.js
@@ -28,7 +28,7 @@ connectDB();
 // CORS
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173", "http://localhost:5174","hallaghar.vercel.app"],
     credentials: true,
   })
 );
